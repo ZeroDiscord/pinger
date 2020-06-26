ab2dce29f32e6a89e5be8d39cfea37a1061c68cf
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-06-26 09:32:59 +0800

    ok

--- src/server.js
+++ src/server.js
@@ -4,19 +4,19 @@ const express = require('express');
 
 const app = express();
 
-let status = 200;
+let status = 404;
 
 app.get('/', (req, res) => {
     res.status(status).send('HEY!');
     console.log('Ping!');
 });
 
-const port = process.env.PORT || 4200
+const port = process.env.PORT || 4200;
 
 app.listen(port);
 console.log(port);
 setInterval(async () => {
-    const response = await fetch('https://oadpoaw-unicron-bot-6.glitch.me/');
+    const response = await fetch('http://49.12.82.83:25575/');
     console.log(`Status - ${response.status}`);
     status = response.status;
-}, 28e4);
+}, 60e3);
